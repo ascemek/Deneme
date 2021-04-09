248100a696a228faa47aae854f23f2948ce70348
ascemek <71249060+ascemek@users.noreply.github.com>
2021-04-09 16:46:17 -0400

    Files Uploaded

---
+++ Assets/Scripts/BalloonController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This one is yours
+
+public class BalloonController : MonoBehaviour
+{
+    public GameObject balloonPrefab;
+    public float floatStrength = 20f;
+    public float growRate = 1.5f;
+
+    private GameObject balloon;
+
+
+    void Update()
+    {
+        //if the trigger button pressed it will create a balloon
+        if (Input.GetButtonDown("XRI_Right_TriggerButton"))
+        {
+            //Debug.Log("Trigger down");
+            CreateBalloon();
+        }
+
+        // if the trigger button released it will release a balloon
+        else if (Input.GetButtonUp("XRI_Right_TriggerButton"))
+        {
+            //Debug.Log("Trigger up");
+            ReleaseBalloon();
+        }
+    }
+
+
+    public void CreateBalloon()
+    {
+        balloon = Instantiate(balloonPrefab);
+        balloon.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+    }
+
+    public void ReleaseBalloon()
+    {
+        Rigidbody rb = balloon.GetComponent<Rigidbody>();
+        Vector3 force = Vector3.up * floatStrength;
+        rb.AddForce(force);
+
+        GameObject.Destroy(balloon, 10f);
+        balloon = null;
+    }
+
+    public void GrowBalloon()
+    {
+        balloon.transform.localScale += balloon.transform.localScale * growRate * Time.deltaTime;
+    }
+}
---
+++ Assets/TemplateScripts/Duplicate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duplicate : MonoBehaviour
+{
+    public GameObject objectOriginal;
+    public GameObject objectContainer;
+
+    void Start()
+    {
+        DuplicateObject(1);
+    }
+
+    public void DuplicateObject(int objectNum)
+    {
+        for (int i = 0; i < objectNum; i++)
+        {
+            GameObject objectClone = Instantiate(objectOriginal, new Vector3(i * 0.6f, objectOriginal.transform.position.y, i * 0.75f), objectOriginal.transform.rotation);
+            objectClone.transform.parent = objectContainer.transform;
+            objectClone.name = "objectClone" + (i + 1);
+        }
+    }
+
+}
+
+
